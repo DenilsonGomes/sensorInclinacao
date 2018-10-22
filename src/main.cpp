3afc1ad74b28c79e58c24426ade213ab6260c00c
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-10-22 15:24:34 -0300

    Projeto Iniciado

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+/*Autor: Denilson Gomes Vaz da Silva
+* Graduando em Engenharia de Computação
+* Universidade Federal do Ceará - UFC
+*
+Switch Tilt KY-020: Codigo para ler o valor do sensor de inclinação KY-020
+*/
+
+//Bibliotecas
+#include <Arduino.h>
+
+//Variaveis
+int sensor = 8; //Variavel sensor no pino 8
+int valor; //Variavel para valor do sensor
+
+void setup() {
+    pinMode(sensor, INPUT); //Pino sensor como entrada
+    Serial.begin(9600); //Inicia comunicação serial
+}
+
+void loop() {
+    valor = digitalRead(sensor); //Valor recebe a leitura do sensor
+    Serial.println(valor); //Exibe o valor da leitura
+    delay(3000); //Espera 3 segundos
+}
